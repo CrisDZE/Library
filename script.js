1e70480318cba94452dfc4a05b813e28fbe7d12b
CrisDZE <150843837+CrisDZE@users.noreply.github.com>
2024-03-07 01:41:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DOM elements
 const mainContainer = document.getElementById("library-main-container")
 
 const nameInput = document.getElementById("add-book-name")
@@ -6,10 +7,39 @@ const pagesInput = document.getElementById("add-book-pages")
 const readInput = document.getElementById("add-book-read")
 const addButton = document.getElementById("add-book-button")
 
+//data object save
 const myLibrary = [];
 
+//e.listener to form button
+addButton.addEventListener('click', function () {
+  //calls to add the form to the data
+  addBookToLibrary()
+  //cleans the DOM and set the complete data array
+  renderLibrary()
+})
+//buttons inside mainContainer(remove and read status)
+mainContainer.addEventListener('click', function (event) {
+  //checks if the button is the remove
+  if (event.target.classList.contains('remove-book-button')) {
+    //takes the index of the button (same as the object)
+    const indexToRemove = event.target.dataset.index;
+    //takes the the index and removes it from the data save (object index and data save position is the same)
+    myLibrary.splice(indexToRemove, 1);
+    //call to render the new array
+    renderLibrary();
+    //checks if the button is the read status
+  } else if (event.target.classList.contains('toggle-read-button')) {
+    //takes the index of the button (same as the object)
+    const indexToToggle = event.target.dataset.index;
+    //uses the func inside the data save status with the button index
+    myLibrary[indexToToggle].toggleReadStatus();
+    //call to render the new array
+    renderLibrary();
+  }
+});
+
+//Book object constructor
 function BookConstructor(title, author, pages, read) {
-  // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -18,40 +48,29 @@ function BookConstructor(title, author, pages, read) {
   this.info = () =>`${this.title} by ${this.author}. ${this.pages} pages. 
     ${this.read ?"Read":"Not Read"}`;
   
+  //changes read status
   this.toggleReadStatus = () => {
     this.read = !this.read;
   };
 };
-
+//Adds new object to the data array
 function addBookToLibrary() {
-  // do stuff here
+  //use values of DOM form to make a new object with the constructor
   const newBook = new BookConstructor(nameInput.value, authorInput.value, pagesInput.value, readInput.value);
+  //adds new key with the array length as value. This works as an unique ID for the object
   newBook.index = myLibrary.length;
+  //adds the new object to the data
   myLibrary.push(newBook);
+  //call to render the div
   renderLibrary();
 }
-
-addButton.addEventListener('click', function () {
-  addBookToLibrary()
-  mainContainer.innerHTML = ''
-for(let i=0; i < myLibrary.length; i++){
-  mainContainer.innerHTML +=`
-  <div id="book-container">
-    <img src="https://img.freepik.com/free-vector/beautiful-book-club-pattern-illustration_23-2149330102.jpg?size=338&ext=jpg&ga=GA1.1.1395880969.1709510400&semt=ais">
-      <p>Title: ${myLibrary[i].title}</p>
-      <p>Author: ${myLibrary[i].author}</p>
-      <p>Pages: ${myLibrary[i].pages} </p>
-      <p>Read: ${myLibrary[i].read ? "Yes" : "No"}</p>
-      <p>${myLibrary[i].info()}</p>
-      <button class="remove-book-button" data-index="${i}">Remove</button>
-  </div>
-  `
-}
-  console.log(myLibrary)
-})
+//renders the data array
 function renderLibrary() {
-  mainContainer.innerHTML = ''; // Limpiar el contenedor
+  //cleans the DOM
+  mainContainer.innerHTML = '';
+  //inherits in the data array
   for (let i = 0; i < myLibrary.length; i++) {
+    //sets new HTML in the DOM
     mainContainer.innerHTML += `
       <div id="book-container">
         <img src="https://img.freepik.com/free-vector/beautiful-book-club-pattern-illustration_23-2149330102.jpg?size=338&ext=jpg&ga=GA1.1.1395880969.1709510400&semt=ais">
@@ -66,16 +85,3 @@ function renderLibrary() {
     `;
   }
 }
-mainContainer.addEventListener('click', function (event) {
-  if (event.target.classList.contains('remove-book-button')) {
-    const indexToRemove = event.target.dataset.index;
-    myLibrary.splice(indexToRemove, 1);
-    renderLibrary();
-  } else if (event.target.classList.contains('toggle-read-button')) {
-    const indexToToggle = event.target.dataset.index;
-    myLibrary[indexToToggle].toggleReadStatus(); // Cambiar el estado de lectura del libro
-    renderLibrary(); // Volver a renderizar la biblioteca después de cambiar el estado de lectura
-  }
-});
-
-
